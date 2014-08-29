6af120657fea470d92e3a75bcd2eb0a184a0dc29
smeyer <smeyer@envision-creative.com>
2014-08-29 14:10:06 -0500

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
 
     sass: {
       options: {
-        includePaths: ['bower_components/foundation/scss']
+        includePaths: ['../FoundationPress/bower_components/foundation/scss']
       },
       dist: {
         options: {
