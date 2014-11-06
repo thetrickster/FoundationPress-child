342eabb7a192c4b9da7cba0732823b3e28a19175
Chris Matthias <chris.matthias@gmail.com>
2014-11-06 14:37:31 -0500

    changing jquery location for wp_enqueue_script-- although considering not deregistering wordpress jquery for future versions. Seems its not good practice.

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -7,7 +7,7 @@ function FoundationPress_scripts() {
 
   // enqueue modernizr, jquery and foundation
   wp_enqueue_script( 'modernizr', get_stylesheet_directory_uri() . '/js/modernizr/modernizr.min.js', array(), '1.0.0', false );
-  wp_enqueue_script( 'jquery', get_stylesheet_directory_uri() . '/js/jquery/jquery.min.js', array(), '1.0.0', true );
+  wp_enqueue_script( 'jquery', get_stylesheet_directory_uri() . '/js/jquery/dist/jquery.min.js', array(), '1.0.0', true );
   wp_enqueue_script( 'foundation', get_stylesheet_directory_uri() . '/js/app.js', array('jquery'), '1.0.0', true );
 
 }
